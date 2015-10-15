ea10f847cf1618a1d1cf06577027c7ceead71cec
John Trent <johnt@hzdg.com>
2015-10-15 13:44:59 -0400

    Add babel and react plugins

--- JavaScript (eslint)/eslint-config-hzdg/index.js
+++ JavaScript (eslint)/eslint-config-hzdg/index.js
@@ -1,10 +1,12 @@
 module.exports = {
   extends: [
+    'eslint-config-hzdg/rules/babel',
     'eslint-config-hzdg/rules/best-practices',
     'eslint-config-hzdg/rules/ecma-features',
     'eslint-config-hzdg/rules/legacy',
     'eslint-config-hzdg/rules/node',
     'eslint-config-hzdg/rules/possible-errors',
+    'eslint-config-hzdg/rules/react',
     'eslint-config-hzdg/rules/strict',
     'eslint-config-hzdg/rules/style',
     'eslint-config-hzdg/rules/variables',
---
+++ JavaScript (eslint)/eslint-config-hzdg/rules/babel.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    'babel',
+  ],
+  rules: {
+    'babel/object-shorthand': 0,
+    'babel/generator-star-spacing': [2, {before: true, after: false}],
+  },
+};
