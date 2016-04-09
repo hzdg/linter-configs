4db90d5e1293b575c9375ab631b4b48006ae6481
John Trent <johnt@hzdg.com>
2016-04-09 12:30:33 -0400

    Upgrade eslint/plugins and Add missing rules

--- JavaScript (eslint)/eslint-config-hzdg/rules/babel.js
+++ JavaScript (eslint)/eslint-config-hzdg/rules/babel.js
@@ -3,7 +3,9 @@ module.exports = {
     'babel',
   ],
   rules: {
+    'babel/array-bracket-spacing': [2, 'never'],
     'babel/arrow-parens': [2, 'as-needed'],
+    'babel/flow-object-type': [2, 'comma'],
     'babel/generator-star-spacing': [2, {before: true, after: false}],
     'babel/new-cap': 0,
     'babel/no-await-in-loop': 2,
