bf134a0572f0847c723409da17493656998893e7
John Trent <johnt@hzdg.com>
2016-05-31 13:18:09 -0400

    Enable jsx-a11y rules

--- JavaScript (eslint)/eslint-config-hzdg/index.js
+++ JavaScript (eslint)/eslint-config-hzdg/index.js
@@ -1,12 +1,13 @@
 module.exports = {
   extends: [
-    'eslint-config-hzdg/rules/plugins/babel',
     'eslint-config-hzdg/rules/best-practices',
     'eslint-config-hzdg/rules/ecma-features',
     'eslint-config-hzdg/rules/legacy',
     'eslint-config-hzdg/rules/node',
-    'eslint-config-hzdg/rules/possible-errors',
+    'eslint-config-hzdg/rules/plugins/babel',
+    'eslint-config-hzdg/rules/plugins/jsx-a11y',
     'eslint-config-hzdg/rules/plugins/react',
+    'eslint-config-hzdg/rules/possible-errors',
     'eslint-config-hzdg/rules/strict',
     'eslint-config-hzdg/rules/style',
     'eslint-config-hzdg/rules/variables',
