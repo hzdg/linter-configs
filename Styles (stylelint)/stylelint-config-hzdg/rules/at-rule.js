2be6f90bd928a6fdbb9935a52a3491960d1b4c0b
John Trent <johnt@hzdg.com>
2016-07-26 17:54:13 -0400

    Disable at-rule rules
    * at-rule-empty-line-before
    * at-rule-no-unknown

--- Styles (stylelint)/stylelint-config-hzdg/rules/at-rule.js
+++ Styles (stylelint)/stylelint-config-hzdg/rules/at-rule.js
@@ -1,10 +1,8 @@
 module.exports = {
   rules: {
     'at-rule-blacklist': ['extend'],
-    'at-rule-empty-line-before': 'always',
     'at-rule-name-case': 'lower',
     'at-rule-name-space-after': 'always-single-line',
-    'at-rule-no-unknown': true,
     'at-rule-no-vendor-prefix': true,
     'at-rule-semicolon-newline-after': 'always',
   },
