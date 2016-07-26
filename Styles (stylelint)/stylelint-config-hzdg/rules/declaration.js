d9c1b6eaf8f8df39f630f3c1a274130244b99194
John Trent <johnt@hzdg.com>
2016-07-26 17:40:00 -0400

    Update declaration rules

--- Styles (stylelint)/stylelint-config-hzdg/rules/declaration.js
+++ Styles (stylelint)/stylelint-config-hzdg/rules/declaration.js
@@ -3,6 +3,7 @@ module.exports = {
     'declaration-colon-newline-after': 'always-multi-line',
     'declaration-colon-space-after': 'always-single-line',
     'declaration-colon-space-before': 'never',
+    'declaration-no-important': true,
     'declaration-property-value-blacklist': {
       '/^transition/': [
         '/all/',
