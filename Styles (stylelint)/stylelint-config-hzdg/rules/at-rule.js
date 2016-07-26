c37d8a3de16ff0d6065eff291147ec3bced69d37
John Trent <johnt@hzdg.com>
2016-07-26 17:38:39 -0400

    Update at-rule rules

--- Styles (stylelint)/stylelint-config-hzdg/rules/at-rule.js
+++ Styles (stylelint)/stylelint-config-hzdg/rules/at-rule.js
@@ -1,8 +1,11 @@
 module.exports = {
   rules: {
-    "at-rule-blacklist": ["extend"],
-    "at-rule-name-case": "lower",
-    "at-rule-name-space-after": "always-single-line",
-    "at-rule-semicolon-newline-after": "always",
+    'at-rule-blacklist': ['extend'],
+    'at-rule-empty-line-before': 'always',
+    'at-rule-name-case': 'lower',
+    'at-rule-name-space-after': 'always-single-line',
+    'at-rule-no-unknown': true,
+    'at-rule-no-vendor-prefix': true,
+    'at-rule-semicolon-newline-after': 'always',
   },
 };
