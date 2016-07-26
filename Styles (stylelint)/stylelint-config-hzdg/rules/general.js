541d89b99b3dbb6e2179eb12672762d436dba3ba
John Trent <johnt@hzdg.com>
2016-07-26 18:00:26 -0400

    Disable no-descending-specificity rule

--- Styles (stylelint)/stylelint-config-hzdg/rules/general.js
+++ Styles (stylelint)/stylelint-config-hzdg/rules/general.js
@@ -1,7 +1,6 @@
 module.exports = {
   rules: {
     'max-nesting-depth': 2,
-    'no-descending-specificity': true,
     'no-duplicate-selectors': true,
     'no-eol-whitespace': true,
     'no-extra-semicolons': true,
