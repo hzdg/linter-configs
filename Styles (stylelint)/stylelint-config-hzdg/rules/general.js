2f8a4eb23861c4ee3ee88c691cf903a827f00d2f
John Trent <johnt@hzdg.com>
2016-07-26 17:40:24 -0400

    Update general rules

--- Styles (stylelint)/stylelint-config-hzdg/rules/general.js
+++ Styles (stylelint)/stylelint-config-hzdg/rules/general.js
@@ -1,9 +1,11 @@
 module.exports = {
   rules: {
     'max-nesting-depth': 2,
+    'no-descending-specificity': true,
     'no-duplicate-selectors': true,
     'no-eol-whitespace': true,
     'no-extra-semicolons': true,
+    'no-indistinguishable-colors': true,
     'number-leading-zero': 'always',
     'number-no-trailing-zeros': true,
   },
