f0809e39cd3dfafb41e42e6a3c64f983933ec212
John Trent <johnt@hzdg.com>
2016-07-26 17:39:32 -0400

    Update declaration block rules

--- Styles (stylelint)/stylelint-config-hzdg/rules/declaration-block.js
+++ Styles (stylelint)/stylelint-config-hzdg/rules/declaration-block.js
@@ -7,5 +7,8 @@ module.exports = {
         ],
       },
     ],
+    'declaration-block-no-ignored-properties': true,
+    'declaration-block-no-shorthand-property-overrides': true,
+    'declaration-block-single-line-max-declarations': 1,
   },
 };
