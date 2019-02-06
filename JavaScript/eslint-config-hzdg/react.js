a5c5d3d2d0e9a61f1ca6a8cf4d584f8ff32dc91c
Andrey Morozov <andreym@hzdg.com>
2019-02-06 10:29:59 -0500

    react-hooks should be added to the 'plugins' field

--- JavaScript/eslint-config-hzdg/react.js
+++ JavaScript/eslint-config-hzdg/react.js
@@ -3,16 +3,16 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:import/react',
-    'plugin:react-hooks',
     'prettier/react',
   ],
+  plugins: ['react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+  },
   settings: {
     react: {
       version: 'detect',
     },
     linkComponents: [{name: 'Link', linkAttribute: 'to'}],
-  },
-  rules: {
-    'react-hooks/rules-of-hooks': 'error',
   },
 };
