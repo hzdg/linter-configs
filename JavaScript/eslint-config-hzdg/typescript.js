a50407c9237a1776bfcbb94e8bb1431b25a8f1fa
Eric Eldredge <lettertwo@gmail.com>
2019-09-27 16:56:23 -0400

    fix(typescript): set @typescript-eslint/no-non-null-assertion: error
    Fixes #75
    Fixes #74
    Fixes #73
    Fixes #72
    Fixes #68

--- JavaScript/eslint-config-hzdg/typescript.js
+++ JavaScript/eslint-config-hzdg/typescript.js
@@ -31,6 +31,7 @@ module.exports = {
         'no-unused-vars': 'error',
         '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'error',
       },
     },
   ],
