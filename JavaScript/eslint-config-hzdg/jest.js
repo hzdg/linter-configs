410a4ce3ebbee6023d397de8e986e7baccf73764
Eric Eldredge <lettertwo@gmail.com>
2019-11-21 16:37:26 -0500

    fix(jest): set jest warnings to errors
    - set jest/expect-expect: error
    - set jest/no-commented-out-tests: error
    
    fixes #77, #78, #79, #80, #81, #82

--- JavaScript/eslint-config-hzdg/jest.js
+++ JavaScript/eslint-config-hzdg/jest.js
@@ -1,7 +1,9 @@
 module.exports = {
   extends: ['plugin:jest/recommended', 'plugin:jest/style'],
   rules: {
+    'jest/expect-expect': 'error',
     'jest/no-alias-methods': 'error',
+    'jest/no-commented-out-tests': 'error',
     'jest/no-disabled-tests': 'error',
     'jest/no-jasmine-globals': 'error',
   },
